763124e6509d966d492b6b03804f1f0dc8a04471
Joel Harris <joel@fortyau.com>
2021-01-02 17:59:38 -0600

    added some more metrics for context

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,19 @@ class User < ApplicationRecord
         amount_vested: av,
         wallet: account['name'].split(" ")[0],
         current_value:  cv,
+        percent_change: (((cv - av)/av.to_f)*100).round(1),
         difference: cv - av
       }
     end
 
+    gv = account_statuses.map{|av| av[:amount_vested]}.reduce(0, :+)
+    vg = account_statuses.map{|av| av[:current_value]}.reduce(0, :+)
+
     {
+      gross_vested: gv,
+      vested_growth: vg,
       net_profit: account_statuses.map{|av| av[:difference]}.reduce(0, :+),
+      percent_change: (((vg - gv)/gv.to_f)*100).round(1),
       accounts: account_statuses
     }
   end
@@ -32,24 +39,30 @@ class User < ApplicationRecord
   # test data for screen shots
   def test_portfolio_status
     {
-      net_profit: 1000,
+      gross_vested: 1400,
+      vested_growth: 2200,
+      net_profit: 800,
+      percent_change: (((2200 - 1400)/1400.to_f)*100).round(1),
       accounts: [
         {
           amount_vested: 800,
           wallet: 'BTC',
           current_value:  1600,
+          percent_change: (((1600 - 800)/800.to_f)*100).round(1),
           difference: 800
         },
         {
           amount_vested: 400,
           wallet: 'ETH',
           current_value:  600,
+          percent_change: (((600 - 400)/400.to_f)*100).round(1),
           difference: 200
         },
         {
           amount_vested: 200,
           wallet: 'LTC',
           current_value:  0,
+          percent_change: (((0 - 200)/200.to_f)*100).round(1),
           difference: -200
         }
       ]
